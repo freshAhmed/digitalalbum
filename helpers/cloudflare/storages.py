1ab33e3bb458bba50bcd4f956beaff2111e52e3f
Ahmed ali <sertyd37@gmail.com>
2025-12-07 19:55:36 -0300

    add helper directory to code base

---
+++ helpers/cloudflare/storages.py
@@ -0,0 +1,12 @@
+from storages.backends.s3 import S3Storage
+
+class CloudflareStorage(S3Storage):
+    pass
+
+
+class StaticFileStorage(CloudflareStorage):
+    location="static"
+
+
+class MediaFileStorage(CloudflareStorage):
+    location="media"
