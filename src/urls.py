986a7bc13de8ce29bcd052aadd6f06f7031a56cc
Ahmed ali <sertyd37@gmail.com>
2025-12-17 18:02:38 -0300

    modify the urls path by removing the path of Post urls and add it to album urls , and add albums urls file

--- src/urls.py
+++ src/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('posts/',include('Post.urls')),
     path('',include('accounts.urls')),
+    path('album/',include('album.urls')),
+    path('admin/', admin.site.urls),
+
 ]
