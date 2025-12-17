902a73edb15d7a92954e2901fa298b8d5b804a5d
Ahmed ali <sertyd37@gmail.com>
2025-12-17 18:15:55 -0300

    modify the models by add album field to it and modify the path of images to should be uploaded

--- Post/models.py
+++ Post/models.py
@@ -1,23 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 import pathlib
 import logging
 import uuid
+from album.models import albumModel
 # Create your models here.
 log=logging.getLogger(__name__)
-def image_file_upload_handler(instance,filepath):
+
+
+#   log.info(username)
+def image_file_upload_handler(instance,filepath,**kwargs):
    instance_id=instance.id
    if not instance_id :
        instance_id=-1
    filepath=pathlib.Path(filepath).resolve()
    fname=str(uuid.uuid1())
    ext=filepath.suffix
-   return f"album/{fname}{ext}"
+   return f"album/{instance.album.id}/{fname}{ext}" 
+
 
 class Post(models.Model):
     Title=models.CharField(max_length=30,default='')
     description=models.TextField(default='')
     image=models.ImageField(upload_to=image_file_upload_handler,blank=True,null=True)
-
+    album=models.ForeignKey(albumModel,on_delete=models.CASCADE,default='')
     def get_data(self):
         data={
             'Title':self.Title,
