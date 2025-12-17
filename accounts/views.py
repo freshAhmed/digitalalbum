2eca4edaae499ee290fcd9b34e83f5bd5c7a6675
Ahmed ali <sertyd37@gmail.com>
2025-12-17 17:42:19 -0300

    create new directory in response_api directory for html post snippets

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import  AuthenticationForm
+import logging
 
-# Create your views here.
+log=logging.getLogger(__name__)
+def login_view(request,*args,**kwargs):
+    form=AuthenticationForm(request.POST or None)
+    log.info(request.POST)
+    if request.method=="POST":
+     log.info(form.data)
+    return render (request,'accounts/login.html',{'form':form})
+    
+
+
+def logout_view(request,*args,**kwargs):
+    return render(request,'base.html',{})
+
+
+
+def register(request,*args,**kwargs):
+    return render(request,'base.html',{})
