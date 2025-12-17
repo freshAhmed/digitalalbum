34cd86e736399062cdbd91fd61678a10ef59b660
Ahmed ali <sertyd37@gmail.com>
2025-12-17 18:18:18 -0300

    modify the path of get_Post

--- Post/urls.py
+++ Post/urls.py
@@ -2,7 +2,6 @@ from .views import (get_post,add_post,delete_post,home_view,modify_post)
 from django.urls import path 
 urlpatterns=[
  path('add',add_post),
-
  path('remove/<str:id>',delete_post),
  path('edit/<str:id>',modify_post),
  path('<str:albumid>/<str:postid>',get_post),
