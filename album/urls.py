5d4902f308075b24e8372501bb43417ec23bc09b
Ahmed ali <sertyd37@gmail.com>
2025-12-17 19:58:37 -0300

    add album/urls file to code base

--- album/urls.py
+++ album/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path,include
 
-from .views import (create_album ,get_album ,delete_album )
+from .views import (create_album ,get_album ,delete_album,edit_album,get_albums )
 
 
 urlpatterns=[
     path('add',create_album),
     path('remove/<str:albumid>',delete_album),
     path('<str:albumid>',get_album),
+    path('edit/<str:albumid>',edit_album),
+    path('',get_albums),
+    path('posts/',include('Post.urls'))
+
 ]
